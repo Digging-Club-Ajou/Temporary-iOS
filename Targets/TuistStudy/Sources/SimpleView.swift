2991b2d0a41ab7aa1530c11cf86a2703ff117d81
99Page <nou0ggid@gmail.com>
2023-08-21 20:30:35 +0900

    create simple view

---
+++ Targets/TuistStudy/Sources/SimpleView.swift
@@ -0,0 +1,21 @@
+//
+//  SimpleView.swift
+//  TuistStudy
+//
+//  Created by 노우영 on 2023/08/21.
+//  Copyright © 2023 tuist.io. All rights reserved.
+//
+
+import SwiftUI
+
+struct SimpleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SimpleView_Previews: PreviewProvider {
+    static var previews: some View {
+        SimpleView()
+    }
+}
